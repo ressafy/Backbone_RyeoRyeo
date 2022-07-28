a1a90e844087ad732f304cf20ac84d568f7de534
Ryeohwan <ahnrh951019@naver.com>
2022-07-29 01:12:09 +0900

    code: 10-2 solve

---
+++ HW/ssafy_hw/src/파일입출력/NameNotFoundException.java
@@ -0,0 +1,14 @@
+package 파일입출력;
+
+public class NameNotFoundException extends Exception{
+	private String name;
+	
+	public NameNotFoundException(String name) {
+		super(name + " 이름을 포함하는 사용자가 존재하지 않습니다.");
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
