97b578cda5273487e90c8fe6126f910161d01670
Ryeohwan <ahnrh951019@naver.com>
2022-08-01 20:01:40 +0900

    code: I start inheritance and polymorphism

---
+++ second_study/src/상속과다형성/김모씨/dish.java
@@ -0,0 +1,18 @@
+package 상속과다형성.김모씨;
+
+public class dish {
+	private boolean steamed = false;
+	private final String size = "midium";
+	
+	public boolean isSteamed() {
+		return steamed;
+	}
+	public void setSteamed(boolean steamed) {
+		this.steamed = steamed;
+	}
+	public String getSize() {
+		return size;
+	}
+	
+	
+}
---
+++ second_study/src/상속과다형성/김모씨/electric.java
@@ -0,0 +1,6 @@
+package 상속과다형성.김모씨;
+
+public interface electric {
+	public abstract void wash();
+	
+}
