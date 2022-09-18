0acab61eb9d3e750e29f6a724c6b9f13581d7030
Ryeohwan <ahnrh951019@naver.com>
2022-09-18 22:45:49 +0900

    code: check commit

--- HW/algojava/안려환/src/week6_안려환/등굣길.java
+++ HW/algojava/안려환/src/week6_안려환/등굣길.java
@@ -2,47 +2,40 @@ package week6_안려환;
 
 public class 등굣길 {
 
-	static int[][] arr;
-	static int[][] puds;
-	public static void main(String[] args) {
-		int width = 4;
-		int height = 3;
-		int[][] pud = new int[][] {{2,2},{1,2}};
-		
-		arr = new int[height][width];
-		puds = new int[height][width];
-		
-		arr[0][0] = 1;
-		
-		for(int i=0; i<pud.length; i++) {
-			int[] temp = pud[i];
-			puds[temp[1]-1][temp[0]-1] = 1;
-		}
-		int answer = 0;
-		
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr[0].length; j++) {
-				if(i==0 && j==0) {
-					continue;
-				}
-				
-				if(puds[i][j] == 1	) {
-					arr[i][j] = 0;
-				}else {
-					if(i==0) {
-						arr[i][j] = arr[i][j-1];
-					}else if(j==0) {
-						arr[i][j] = arr[i-1][j];
-					}else {
-						arr[i][j] = (arr[i-1][j] + arr[i][j-1])%1000000007;
-					}
-				}
-			}
-		}
-		
-		answer = arr[arr.length-1][arr[0].length-1];
-		System.out.println(answer);
-		
-	}
+	public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n + 1][m + 1];
+        dp[1][1] = 1;
+        
+        // 웅덩이 초기화
+        for (int i = 0; i < puddles.length; i++) {
+            int x = puddles[i][1];
+            int y = puddles[i][0];
+            dp[x][y] = -1;
+        }
+        
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (dp[i][j] == -1) {
+                    dp[i][j] = 0;
+                    continue;  // 웅덩이는 0으로 해줘서 점화식 사용할 수 있게 한다.
+                }
+                
+                if (!(i == 1 && j == 1)) {  // 시작 점이 아닐 때
+                    int left = 0;
+                    int up = 0;
+                 // 각 1인 경우 맨 위와 맨 왼쪽이기 때문에 맨 위나 왼쪽은 좌 우 중 한 쪽이 없다.
+                    if (j > 1)   
+                        left = dp[i][j - 1];
+                    if (i > 1) 
+                        up = dp[i - 1][j];
 
+                    dp[i][j] = (left + up) % 1000000007;
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
 }
+	
+
