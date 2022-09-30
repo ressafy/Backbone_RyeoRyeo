7a30b6fe156ce6945690381f70565cb6707f3f04
= <bora2292@naver.com>
2022-09-30 17:53:36 +0900

    coode: add 0930

---
+++ BaekJoon_course/Dynamic_Programming/src/BaekJoon/P068_SWEA1249_보급로.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class P068_SWEA1249_보급로 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int C = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= C; i++) {
+			int N = Integer.parseInt(br.readLine());
+			int NumList [] = new int[N];
+			for (int j = 0; j < N; j++) {
+				String numArr = br.readLine();
+				char [] arr = numArr.toCharArray();
+			}
+			
+			
+		}
+	}
+
+}
