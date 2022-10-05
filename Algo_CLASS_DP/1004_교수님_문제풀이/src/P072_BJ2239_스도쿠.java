7246c5b38fb9a8dddf2ceeaae049e96eba856f88
안려환 <arh951019@gmail.com>
2022-10-05 17:54:53 +0900

    code: add 1005

--- Algo_CLASS_DP/1004_교수님_문제풀이/src/P072_BJ2239_스도쿠.java
+++ Algo_CLASS_DP/1004_교수님_문제풀이/src/P072_BJ2239_스도쿠.java
@@ -1,14 +1,17 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class P072_BJ2239_스도쿠 {
 	private static int [][] map;
-	private int check;
-	private static List<Pos> list = new ArrayList<Pos>() ;
+	private static int check = 0, cnt = 0;
+	private static int bx,by;
+	private static List<Pos> list = new ArrayList<>();
 	
-	static class Pos{
+	private static class Pos{
 		int x,y;
 
 		public Pos(int x, int y) {
@@ -29,36 +32,46 @@ public class P072_BJ2239_스도쿠 {
 			for (int j = 0; j < 9; j++) {
 				map[i][j] = temp.charAt(j)-'0';
 				if(map[i][j] == 0) {
+					cnt++; 
 					list.add(new Pos(j,i));
-				}
+				} 
 			}
 		}  // 입력부 완
-		
-		for(Pos p : list) {
-			sudoku(0,p.x,p.y);
-			printMap();
-		}
-		
+		Pos a = list.get(0);
+		int x = a.x;
+		int y = a.y;
+		sudoku(1,x,y,0);
+		System.out.println(Arrays.deepToString(map));
 	}
 	
-	private static void printMap() {  // 출력부 , 근데 81번째 수가 가장 작은 map을 출력해야 한다....
+	
+	private static void sudoku(int start, int x, int y, int time) {  // 여기서 스도쿠 판을 채워준다.
+		// 기저 조건 
+		if(time == cnt) return;
 		
-	}
-
-	private static void sudoku(int cnt,int x, int y) {  // 여기서 스도쿠 판을 채워준다.
-		// 맵을 돌면서 0이 나오면 
-		if() return;
-		for (int i = 1; i <= 9; i++) {
+		for (int i = start; i <= 9; i++) {
 			if(check(x,y,i)) {
-				 map[y][x] = i;
-				 if(x+1<9) sudoku(cnt,x+1,y);
-				 
-				 
+				if(time < cnt) {
+					System.out.println("go: "+time);
+					map[y][x] = i;
+					Pos a = list.get(time+1);
+					int nx = a.x;
+					int ny = a.y;
+					sudoku(1,nx,ny,time+1);
+				}
 			}
-			
+		}
+		if(map[y][x] == 0) {
+			System.out.println("back: "+time);
+			int nx = list.get(time-1).x;
+			int ny = list.get(time-1).y;
+			sudoku(1,nx,ny,time-1);
 		}
 		
+		
+		time++;
 	}
+	
 	
 	// 유효성 검사
 	private static boolean check(int x, int y, int num) {  // 맵 안의 수를 가지고 check 
@@ -89,4 +102,18 @@ public class P072_BJ2239_스도쿠 {
 		
 		return true;
 	}
+	
+	private static boolean allcheck() {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if(map[i][j] == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	
+	
 }
