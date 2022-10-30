bf89475749375f845d37d41068966f6530f0bf88
안려환 <arh951019@gmail.com>
2022-10-30 20:12:27 +0900

    code: start study problem

---
+++ for_type_a/self_study/src/스터디문제풀이/카드섞기.java
@@ -0,0 +1,21 @@
+package 스터디문제풀이;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 카드섞기 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < 2; i++) {
+			String [] arr = br.readLine().split(" ");
+			for (int j = 0; j < N; j++) {
+				
+			}
+		}
+			
+	}
+
+}
