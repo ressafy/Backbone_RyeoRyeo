5c4600ca5ef530e1ec3e1ed7b76c3a8097c0caea
안려환 <arh951019@gmail.com>
2022-11-01 20:44:31 +0900

    code: 배틀필드 start

---
+++ for_type_a/self_study/src/구현/상호의배틀필드.java
@@ -0,0 +1,16 @@
+package 구현;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 상호의배틀필드 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			
+		}
+	}
+
+}
