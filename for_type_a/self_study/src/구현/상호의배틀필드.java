d6bf7d2d0b1ce6bf9943be5a69c91bff9feade3d
안려환 <arh951019@gmail.com>
2022-11-02 21:23:49 +0900

    code: 상호 마무리

--- for_type_a/self_study/src/구현/상호의배틀필드.java
+++ for_type_a/self_study/src/구현/상호의배틀필드.java
@@ -7,28 +7,32 @@ import java.util.Arrays;
 public class 상호의배틀필드 {
 	private static char [][] map;
 	private static StringBuilder sb = new StringBuilder();
+	private static int H,W;
+	private static Pos po;
 	private static class Pos{
-		int j,i,dir;
+		int i,j,dir;
 		// 0 : up 1: down 2: left 3: right
 		
-		public Pos(int j, int i, int dir) {
+		public Pos(int i, int j, int dir) {
 			super();
-			this.j = j;
 			this.i = i;
+			this.j = j;
 			this.dir = dir;
 		}
 		
 	}
+	
+	
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
 			String [] arr1 = br.readLine().split(" ");
-			int	H = Integer.parseInt(arr1[0]);
-			int W = Integer.parseInt(arr1[1]);
+			H = Integer.parseInt(arr1[0]);
+			W = Integer.parseInt(arr1[1]);
 			map = new char [H][W];
-			Pos po = new Pos(0,0,0);
+			po = new Pos(0,0,0);
 			for (int i = 0; i < H; i++) {
 				String arr = br.readLine();
 				for (int j = 0; j < W; j++) {
@@ -36,40 +40,45 @@ public class 상호의배틀필드 {
 					if(arr.charAt(j) != '.' && arr.charAt(j) != '-' && arr.charAt(j) != '*' && arr.charAt(j) != '#') {
 						switch(arr.charAt(j)) {
 						case '^':
-							po = new Pos(j,i,0);
+							po = new Pos(i,j,0);
 							break;
 						case 'v':
-							po = new Pos(j,i,1);
+							po = new Pos(i,j,1);
 							break;
 						case '<':
-							po = new Pos(j,i,2);
+							po = new Pos(i,j,2);
 							break;
 						case '>':
-							po = new Pos(j,i,3);
+							po = new Pos(i,j,3);
 							break;
 						}
 					}
 				}
-			}
-			System.out.println(po.i+" "+po.j+" "+po.dir);
+			}  // 맵 입력부 완
+			
+//			for (int j = 0; j < H; j++) {
+//				System.out.println(map[j]);
+//			}
+			
 			int N = Integer.parseInt(br.readLine());
 			String mov = br.readLine();
 			for (int i = 0; i < N; i++) {
 				switch (mov.charAt(i)) {
 				case 'S':  // 포탄 발사
-					
+					// 바라보는 방향 으로 탄을 쏘는데 
+					shoot();
 					break;
 				case 'U':  // 위로 바꾸고 평지라면 이동
-					
+					move(0);
 					break;
 				case 'R':  // 오른쪽 
-	
+					move(3);
 					break;
 				case 'L':  // 왼쪽
-	
+					move(2);
 					break;
 				case 'D':  // 아래쪽
-					
+					move(1);
 					break;
 				default:
 					break;
@@ -78,9 +87,111 @@ public class 상호의배틀필드 {
 			
 			sb.append("#").append(tc).append(" ");
 			for (int i = 0; i < H; i++) {
-				sb.append(map[i]).append("");
+				sb.append(map[i]).append("\n");
 			}
 		}
+		System.out.println(sb);
+	}
+	
+	private static void shoot() {
+		switch(po.dir) {
+		case 0:
+			for (int i = po.i-1; i >= 0 ; i--) {
+				if(map[i][po.j] == '*') {
+					map[i][po.j] = '.';
+					break;
+				}else if(map[i][po.j] == '#') {
+					break;
+				}
+			}
+			break;
+		case 1:
+			for (int i = po.i+1; i < H; i++) {
+				if(map[i][po.j] == '*') {
+					map[i][po.j] = '.';
+					break;
+				}else if(map[i][po.j] == '#') {
+					break;
+				}
+			}
+			break;
+		case 2:
+			for (int j = po.j-1; j >= 0; j--) {
+				if(map[po.i][j] == '*') {
+					map[po.i][j] = '.';
+					break;
+				}else if(map[po.i][j] == '#') {
+					break;
+				}
+			}
+			break;
+		case 3: 
+			for (int j = po.j+1; j < W; j++) {
+				if(map[po.i][j] == '*') {
+					map[po.i][j] = '.';
+					break;
+				}else if(map[po.i][j] == '#') {
+					break;
+				}
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	
+	private static void move(int n) {
+		// 주어진 방향으로 돌리면 되니까 현재 방향은 중요하지 않다.
+		switch(n) {
+		case 0:
+			if(po.i-1 >= 0 && map[po.i-1][po.j] == '.') {
+				map[po.i][po.j] = '.';
+				po = new Pos(po.i-1,po.j,0);
+				map[po.i][po.j] = '^';
+				break;
+			}else {
+				po = new Pos(po.i,po.j,0);
+				map[po.i][po.j] = '^';
+				break;
+			}
+		case 1:
+			if(po.i+1 < H && map[po.i+1][po.j] == '.') {
+				map[po.i][po.j] = '.';
+				po = new Pos(po.i+1,po.j,1);
+				map[po.i][po.j] = 'v';
+				break;
+			}else {
+				po = new Pos(po.i,po.j,1);
+				map[po.i][po.j] = 'v';
+				break;
+			}
+		case 2:
+			if(po.j-1 >= 0 && map[po.i][po.j-1] == '.') {
+				map[po.i][po.j] = '.';
+				po = new Pos(po.i,po.j-1,2);
+				map[po.i][po.j] = '<';
+				break;
+			}else {
+				po = new Pos(po.i,po.j,2);
+				map[po.i][po.j] = '<';
+				break;
+			}
+		case 3: 
+			if(po.j+1 < W && map[po.i][po.j+1] == '.') {
+				map[po.i][po.j] = '.';
+				po = new Pos(po.i,po.j+1,3);
+				map[po.i][po.j] = '>';
+				break;
+			}else {
+				po = new Pos(po.i,po.j,3);
+				map[po.i][po.j] = '>';
+				break;
+			}
+		default:
+			break;
+		}
+		
+		
 	}
 
 }
